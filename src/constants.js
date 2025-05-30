4ab480a4126bba959173241bb92ef99ebad53987
Gangadhar184 <zenistuig65@gmail.com>
2025-05-30 16:25:00 +0530

    added env file

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const COUNTRIES =" https://restcountries.com/v3.1/all"
 
-export const WEATHER_API_KEY = "89e769744615f0e81c926a078d54655f";
+export const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_KEY;
 
